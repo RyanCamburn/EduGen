652f20fa970a6a0c5f71a11717603e5505b97c24
RyanCamburn <ryancamburn03@gmail.com>
2024-11-04 21:49:39 -0500

    Altered the script to write the transcription to a text file, so that it can be fed to a RAG LLM as a document

--- audio_to_text.py
+++
@@ -1,7 +0,0 @@
-# Before running, run command in terminal:
-# pip install -U openai-whisper
-import whisper
-
-model = whisper.load_model("turbo")
-result = model.transcribe("./sample.mp3")
-print(result["text"])
